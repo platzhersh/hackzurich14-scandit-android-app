07ca6d6c42686592da2e2aeb127e67da77bcef3c
U-berzerker2\chregi <chregi@berzerker2.(none)>
2014-10-11 13:18:40 +0200

    GUI adjustments

--- gen/com/mirasense/scanditsdkdemo/R.java
+++ gen/com/mirasense/scanditsdkdemo/R.java
@@ -21,19 +21,15 @@ public final class R {
     }
     public static final class id {
         public static final int FrameLayout01=0x7f080000;
-        public static final int cropped_button=0x7f080007;
-        public static final int fullscreen_button=0x7f080004;
-        public static final int link=0x7f080003;
+        public static final int fullscreen_button=0x7f080002;
         public static final int root=0x7f080001;
-        public static final int scaled_button=0x7f080006;
-        public static final int seekBarPrefBarContainer=0x7f08000d;
-        public static final int seekBarPrefUnitsLeft=0x7f08000c;
-        public static final int seekBarPrefUnitsRight=0x7f08000a;
-        public static final int seekBarPrefValue=0x7f08000b;
-        public static final int settings=0x7f080008;
-        public static final int small_buttons=0x7f080005;
-        public static final int titleLayout=0x7f080009;
-        public static final int version=0x7f080002;
+        public static final int seekBarPrefBarContainer=0x7f080009;
+        public static final int seekBarPrefUnitsLeft=0x7f080008;
+        public static final int seekBarPrefUnitsRight=0x7f080006;
+        public static final int seekBarPrefValue=0x7f080007;
+        public static final int settings=0x7f080004;
+        public static final int small_buttons=0x7f080003;
+        public static final int titleLayout=0x7f080005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
